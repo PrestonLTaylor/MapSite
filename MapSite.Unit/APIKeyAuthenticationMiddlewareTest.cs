26a71d3215b03d682bad36bae5d0077c76dbf95e
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-04-12 20:32:38 +0100

    Add Middleware for authorising API requests with a key
    We check if the API_KEY header is set in the request header. If it is
    the valid API key then the request is authorised. Otherwise, it will
    return unauthorised.
    
    This middleware only runs on paths starting with "/api".

---
+++ MapSite.Unit/APIKeyAuthenticationMiddlewareTest.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using MapSite.Middleware;
+using MapSite.Services;
+using System.Net;
+
+namespace MapSite.Unit;
+
+internal sealed class APIKeyAuthenticationMiddlewareTest
+{
+    [Test]
+    public async Task Middleware_ReturnsUnauthorised_WhenRequestHasNoAPIKeyHeader()
+    {
+        // Arrange
+        const string APIPath = "/api";
+        const string ValidAPIKey = "VALID-KEY";
+        using var host = await StartHostWithMiddlewareWithAPIKeyAsync(ValidAPIKey);
+
+        // Act
+        var response = await host.GetTestClient().GetAsync(APIPath);
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+    }
+
+    [Test]
+    public async Task Middleware_ReturnsUnauthorised_WhenRequestHasInvalidAPIKey()
+    {
+        // Arrange
+        const string APIPath = "/api";
+        const string ValidAPIKey = "VALID-KEY";
+        const string InvalidAPIKey = "INVALID-KEY";
+        using var host = await StartHostWithMiddlewareWithAPIKeyAsync(ValidAPIKey);
+        var testClient = SetupTestClientToHaveAPIKey(host, InvalidAPIKey);
+
+        // Act
+        var response = await testClient.GetAsync(APIPath);
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+    }
+
+    [Test]
+    public async Task Middleware_ReturnsNotFound_WhenRequestHasValidAPIKey()
+    {
+        // Arrange
+        const string APIPath = "/api";
+        const string ValidAPIKey = "VALID-KEY";
+        using var host = await StartHostWithMiddlewareWithAPIKeyAsync(ValidAPIKey);
+        var testClient = SetupTestClientToHaveAPIKey(host, ValidAPIKey);
+        // Act
+        var response = await testClient.GetAsync(APIPath);
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+    }
+
+    [Test]
+    public async Task Middleware_ReturnsNotFound_WhenRequestIsNotForAPI()
+    {
+        // Arrange
+        const string Path = "/";
+        const string ValidAPIKey = "VALID-KEY";
+        using var host = await StartHostWithMiddlewareWithAPIKeyAsync(ValidAPIKey);
+
+        // Act
+        var response = await host.GetTestClient().GetAsync(Path);
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+    }
+
+    private async Task<IHost> StartHostWithMiddlewareWithAPIKeyAsync(string apiKey)
+    {
+        return await new HostBuilder().ConfigureWebHost(webBuilder =>
+            {
+                webBuilder.UseTestServer()
+                    .ConfigureAppConfiguration((config) =>
+                    {
+                        var configDictionary = new Dictionary<string, string?>
+                        {
+                            { APIKey.SectionName, apiKey }
+                        };
+
+                        config.AddInMemoryCollection(configDictionary);
+                    })
+                    .ConfigureServices((services) =>
+                    {
+                        services.AddAPIKeyAuthenticationServices();
+                    })
+                    .Configure((app) =>
+                    {
+                        app.UseAPIKeyAuthentication();
+                    });
+            }).StartAsync();
+    }
+
+    private HttpClient SetupTestClientToHaveAPIKey(IHost host, string apiKey)
+    {
+        var testClient = host.GetTestClient();
+        testClient.DefaultRequestHeaders.Add(APIKey.SectionName, apiKey);
+        return testClient;
+    }
+}
---
+++ MapSite.Unit/APIKeyAuthenticatorTest.cs
@@ -0,0 +1,82 @@
+﻿using MapSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+
+namespace MapSite.Unit;
+
+// TODO: Use a test data generator for unit tests
+internal sealed class APIKeyAuthenticatorTest
+{
+    [Test]
+    public void IsRequestAuthenticated_WhenSuppliedInvalidAPIKey_ReturnsFalse()
+    {
+        // Arrange
+        const string InvalidAPIKey = "INVALID-KEY";
+        const string ValidAPIKey = "VALID-KEY";
+        var configuration = CreateConfigurationWithAPIKey(ValidAPIKey);
+        var request = CreateHttpRequestMockWithAPIKey(InvalidAPIKey);
+        var authenticator = CreateAuthenticatorWithConfiguration(configuration);
+
+        // Act
+        var isAuthenticated = authenticator.IsRequestAuthenticated(request.Object);
+
+        // Assert
+        Assert.That(isAuthenticated, Is.False);
+    }
+
+    [Test]
+    public void IsRequestAuthenticated_WhenSuppliedNullAPIKey_ReturnsFalse()
+    {
+        // Arrange
+        const string ValidAPIKey = "VALID-KEY";
+        var configuration = CreateConfigurationWithAPIKey(ValidAPIKey);
+        var request = CreateHttpRequestMockWithAPIKey(null!);
+        var authenticator = CreateAuthenticatorWithConfiguration(configuration);
+
+        // Act
+        var isAuthenticated = authenticator.IsRequestAuthenticated(request.Object);
+
+        // Assert
+        Assert.That(isAuthenticated, Is.False);
+    }
+
+    [Test]
+    public void IsRequestAuthenticated_WhenSuppliedValidAPIKey_ReturnsTrue()
+    {
+        // Arrange
+        const string ValidAPIKey = "VALID-KEY";
+        var configuration = CreateConfigurationWithAPIKey(ValidAPIKey);
+        var request = CreateHttpRequestMockWithAPIKey(ValidAPIKey);
+        var authenticator = CreateAuthenticatorWithConfiguration(configuration);
+
+        // Act
+        var isAuthenticated = authenticator.IsRequestAuthenticated(request.Object);
+
+        // Assert
+        Assert.That(isAuthenticated, Is.True);
+    }
+
+    private IConfiguration CreateConfigurationWithAPIKey(string apiKey)
+    {
+        var configurationDictionary = new Dictionary<string, string?>
+        {
+            { APIKey.SectionName, apiKey }
+        };
+
+        return new ConfigurationBuilder().AddInMemoryCollection(configurationDictionary).Build();
+    }
+
+    private Mock<HttpRequest> CreateHttpRequestMockWithAPIKey(string apiKey)
+    {
+        var requestMock = new Mock<HttpRequest>();
+        requestMock.SetupGet(x => x.Headers[APIKey.SectionName]).Returns(apiKey);
+        return requestMock;
+    }
+
+    private APIKeyAuthenticator CreateAuthenticatorWithConfiguration(IConfiguration configuration)
+    {
+        return new APIKeyAuthenticator(configuration, new NullLogger<APIKeyAuthenticator>());
+    }
+}
