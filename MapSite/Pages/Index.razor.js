8b9c0e9cf4039ce6e3eb5c654ae3f91395856710
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-04-13 19:36:07 +0100

    Add leaflet and initialize it with the stored map entities on load
    For now we hard code configuration values like the map's image and it's
    bounds, etc.

---
+++ MapSite/Pages/Index.razor.js
@@ -0,0 +1,39 @@
+﻿var mapIcons = {};
+var entityMarkers = {};
+var map;
+
+export function InitializeMapIcons(entityTypeToImage) {
+    for (let entityType in entityTypeToImage) {
+        mapIcons[entityType] = L.icon({
+            iconUrl: entityTypeToImage[entityType],
+            iconSize: [64, 64]
+        });
+    }
+}
+
+export function InitializeMapContainer(mapContainerId, boundsConfig, imageOverlay) {
+    map = L.map(mapContainerId, {
+        crs: L.CRS.Simple,
+        minZoom: -5
+    });
+
+    const bounds = [[boundsConfig.topX, boundsConfig.topY], [boundsConfig.bottomX, boundsConfig.bottomX]];
+    L.imageOverlay(imageOverlay, bounds).addTo(map);
+    map.fitBounds(bounds);
+}
+
+function AddMapEntity(mapEntityId, mapEntity) {
+    let icon = mapIcons[mapEntity.entityType];
+    if (icon) {
+        entityMarkers[mapEntityId] = L.marker([mapEntity.position.x, mapEntity.position.y], { icon: mapIcons[mapEntity.entityType]}).addTo(map);
+    } else {
+        entityMarkers[mapEntityId] = L.marker([mapEntity.position.x, mapEntity.position.y]).addTo(map);
+    }
+}
+
+export function AddInitialMapEntities(mapEntities) {
+    for (let mapEntityId in mapEntities) {
+        let mapEntity = mapEntities[mapEntityId];
+        AddMapEntity(mapEntityId, mapEntity);
+    }
+}
